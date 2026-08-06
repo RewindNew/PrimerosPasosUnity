df197bdc71beab6f33102af74075c694be0d905b
RewindNew <jgaleano324@soyudemedellin.edu.co>
2026-08-06 11:36:00 -0500

    Player jump and move

--- Project unity 0/Assets/_Main/Scripts/Player movement.cs
+++ Project unity 0/Assets/_Main/Scripts/Player movement.cs
@@ -3,33 +3,28 @@ using UnityEngine;
 public class Playermovement : MonoBehaviour
 {
     //variables
-
-    public float altura = 1.77f;
-    public int edad = 18;
-    public string nombre = "Juan";
-    public bool puedeVotar = true;
-
-
-    public SpriteRenderer spriteRenderer;
-    public GameObject gameObject;
-    public Rigidbody2D rigidbody2D;
-    public Collider2D collider2D;
-    public Transform transform;
+    [SerializeField] private float _force = 10f;
+[SerializeField] private float _speed = 5f;
+[SerializeField] private Rigidbody2D _rigidbody2D;
+   
 
     private void Start()
     {
-        Debug.Log(" Hola: " + nombre + " tu edad es: " + edad + " tu altura es: " + altura);
-        rigidbody2D. simulated = false;
 
-        spriteRenderer. color =  Color. red;
-
-        transform. position = new Vector3(10,0,0);
+        _rigidbody2D = GetComponent<Rigidbody2D>();
 
     }
 
     private void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody2D.AddForce(Vector2.up * _force);
+
+        }
+        _rigidbody2D.linearVelocity = Vector2.right * _speed * Time.deltaTime;
+     
     }
 
 }
